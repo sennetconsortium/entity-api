1fd34bd0460f96b0664ff11158800bfc400be896
maxsibilla <maxsibilla@icloud.com>
2022-08-30 10:56:33 -0400

    Modifying UUID API URL

--- src/schema/schema_manager.py
+++ src/schema/schema_manager.py
@@ -1101,7 +1101,7 @@ dict
 def get_sennet_ids(id):
     global _uuid_api_url
 
-    target_url = _uuid_api_url + '/' + id
+    target_url = _uuid_api_url + '/uuid/' + id
 
     # Function cache to improve performance
     response = make_request_get(target_url, internal_token_used = True)
@@ -1261,9 +1261,9 @@ def create_sennet_ids(normalized_class, json_data_dict, user_token, user_info_di
     logger.info("======create_sennet_ids() json_to_post to uuid-api======")
     logger.info(json_to_post)
 
-    # uuid_url = _uuid_api_url + "/uuid"
+    uuid_url = _uuid_api_url + "/uuid"
     # Disable ssl certificate verification
-    response = requests.post(url = _uuid_api_url, headers = request_headers, json = json_to_post, verify = False, params = query_parms)
+    response = requests.post(url = uuid_url, headers = request_headers, json = json_to_post, verify = False, params = query_parms)
     
     # Invoke .raise_for_status(), an HTTPError will be raised with certain status codes
     response.raise_for_status()
