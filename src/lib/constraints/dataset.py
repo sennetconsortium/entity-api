12b09ae0554cb387d81aa68eb93544c7d9c78be3
Lisa-Ann B <lisabruney@pitt.edu>
2023-02-24 11:08:48 -0500

    Add comments

--- src/lib/constraints/dataset.py
+++ src/lib/constraints/dataset.py
@@ -1,7 +1,10 @@
 from lib.constraints.base import build_constraint, build_constraint_unit
 
 
+# can be the descendant of / --->
 def build_all_dataset_constraints(entity):
+
+    # Dataset ---> Dataset
     ancestor = build_constraint_unit(entity)
     descendant = build_constraint_unit(entity)
     return [
