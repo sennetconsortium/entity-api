b5ee5beb1260025c1987132ffe9c5c1b9d11ab05
Tyler Madonna <tjm159@pitt.edu>
2023-05-10 11:27:04 -0400

    Add get ancestors test case

--- test/test_app.py
+++ test/test_app.py
@@ -211,3 +211,30 @@ def test_create_entity_invalid(app, entity_type):
                           headers=headers)
                     
         assert res.status_code == 400
+
+### Get Ancestors
+
+@pytest.mark.parametrize('entity_type', [
+    'source',
+    'sample',
+    'dataset',
+])
+def test_get_ancestors_success(app, entity_type):
+    """Test that the get ancestors endpoint returns the correct entity"""
+
+    with open (os.path.join(test_data_dir, f'get_ancestors_success_{entity_type}.json'), 'r') as f:
+        test_data = json.load(f)
+    entity_id = test_data['uuid']
+
+    with (app.test_client() as client,
+          patch('app.auth_helper_instance.getUserInfo', return_value=test_data['getUserInfo']),
+          patch('app.auth_helper_instance.get_default_read_group_uuid', return_value=test_data['get_default_read_group_uuid']),
+          patch('app.schema_manager.get_sennet_ids', return_value=test_data['get_sennet_ids']),
+          patch('app.app_neo4j_queries.get_entity', return_value=test_data['get_entity']),
+          patch('app.app_neo4j_queries.get_ancestors', return_value=test_data['get_ancestors'])):
+
+        res = client.get(f'/ancestors/{entity_id}',
+                         headers=test_data['headers'])
+
+        assert res.status_code == 200
+        assert res.json == test_data['response'] 
