c84a3a789c3cb2303fa4c82f73901c5bea28c40d
Lisa-Ann B <lisabruney@pitt.edu>
2023-08-09 10:11:52 -0400

    Dry Ontology helper class - #166

--- src/lib/ontology.py
+++ src/lib/ontology.py
@@ -1,14 +1,10 @@
 from atlas_consortia_commons.ubkg.ubkg_sdk import UbkgSDK
-from flask import current_app
 
 
 def enum_val_lower(val):
     return val.value.lower()
 
 
+# Custom accessors etc. can be added to the Ontology class
 class Ontology(UbkgSDK):
-    @staticmethod
-    def assay_types_ext():
-        Ontology.ops().Ops.key = 'data_type'
-        Ontology.ops().Ops.url_params = '&dataset_provider=external'
-        return Ontology.ops().transform_ontology(current_app.ubkg.assay_types, 'AssayTypesExt')
+    pass
