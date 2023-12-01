495baaf065791ee755de7eb45c9e8e7ede29adfe
Tyler Madonna <tjm159@pitt.edu>
2023-12-01 14:42:09 -0500

    Changing group name validation to use displayname

--- src/schema/schema_validators.py
+++ src/schema/schema_validators.py
@@ -563,7 +563,7 @@ new_data_dict : dict
 def validate_group_name(property_key, normalized_entity_type, request, existing_data_dict, new_data_dict):
     assigned_to_group_name = new_data_dict['assigned_to_group_name']
     globus_groups = schema_manager.get_auth_helper_instance().getHuBMAPGroupInfo()
-    globus_group = next((v for v in globus_groups.values() if v.get("shortname") == assigned_to_group_name), None)
+    globus_group = next((v for v in globus_groups.values() if v.get("displayname") == assigned_to_group_name), None)
     if globus_group is None:
         raise ValueError("Invalid value for 'assigned_to_group_name'")
     if not globus_group.get("data_provider", False):
