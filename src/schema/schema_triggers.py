190bc0ce689b7390d5576f0e65b986e9e3d389b3
Tyler Madonna <tjm159@pitt.edu>
2024-06-18 16:37:49 -0400

    Using literal_eval for metadata load

--- src/schema/schema_triggers.py
+++ src/schema/schema_triggers.py
@@ -1,3 +1,4 @@
+import ast
 import json
 
 import yaml
@@ -1223,7 +1224,7 @@ def get_cedar_mapped_metadata(property_key, normalized_type, user_token, existin
         # For datasets
         if 'ingest_metadata' not in existing_data_dict:
             return property_key, None
-        ingest_metadata = json.loads(existing_data_dict['ingest_metadata'].replace("'", '"'))
+        ingest_metadata = ast.literal_eval(existing_data_dict['ingest_metadata'])
         if 'metadata' not in ingest_metadata:
             return property_key, None
         metadata = ingest_metadata['metadata']
