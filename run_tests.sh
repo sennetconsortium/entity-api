9cfdf7c278ef4514a9247e1e8ac6979b5809a337
Tyler Madonna <tjm159@pitt.edu>
2024-12-18 13:52:11 -0500

    Creating integration tests for creating entities

---
+++ run_tests.sh
@@ -0,0 +1,51 @@
+# Check if there's a entity-api-test container running, if found, tear it down and start fresh
+if [ "$(docker ps -q -f name=entity-api-test)" ]; then
+    echo "Tearing down the existing entity-api-test container"
+    docker-compose -f docker/docker-compose.test.yml down
+fi
+
+# check if there's a src/instance/app.cfg file, if found, rename it to app.cfg.bak
+if [ -f src/instance/app.cfg ]; then
+    echo "Backing up src/instance/app.cfg to src/instance/app.cfg.bak"
+    cp src/instance/app.cfg src/instance/app.cfg.bak
+
+    echo "Setting environment variables from src/instance/app.cfg"
+    UBKG_SERVER=$(grep UBKG_SERVER src/instance/app.cfg | cut -d"'" -f2)
+    UBKG_ENDPOINT_VALUESET=$(grep UBKG_ENDPOINT_VALUESET src/instance/app.cfg | cut -d"'" -f2)
+    UBKG_CODES=$(grep UBKG_CODES src/instance/app.cfg | cut -d"'" -f2)
+else
+    echo "No src/instance/app.cfg file found"
+    echo "Setting default environment variables"
+fi
+
+# escape the & character
+UBKG_ENDPOINT_VALUESET=$(echo "$UBKG_ENDPOINT_VALUESET" | sed 's/&/\\&/g')
+UBKG_CODES=$(echo "$UBKG_CODES" | sed 's/&/\\&/g')
+
+# copy the src/instance/app.cfg.example to src/instance/app.cfg
+cp src/instance/app.cfg.example src/instance/app.cfg
+
+# search src/instance/app.cfg for the lines that start with NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD
+# replace with the test values
+sed -i "" "s|NEO4J_URI = 'bolt://hubmap-neo4j-localhost:7687'|NEO4J_URI = 'bolt://neo4j-test:7687'|g" src/instance/app.cfg
+sed -i "" "s|NEO4J_PASSWORD = '123'|NEO4J_PASSWORD = None|g" src/instance/app.cfg
+
+# search src/instance/app.cfg for the lines that start with MEMCACHED_MODE and replace with false
+sed -i "" "s|MEMCACHED_MODE = True|MEMCACHED_MODE = False|g" src/instance/app.cfg
+
+# search for UBKG values and replace with test values in environment variables
+sed -i "" "s|UBKG_SERVER =|UBKG_SERVER = '${UBKG_SERVER}'|g" src/instance/app.cfg
+sed -i "" "s|UBKG_ENDPOINT_VALUESET =|UBKG_ENDPOINT_VALUESET = '${UBKG_ENDPOINT_VALUESET}'|g" src/instance/app.cfg
+sed -i "" "s|UBKG_CODES =|UBKG_CODES = '${UBKG_CODES}'|g" src/instance/app.cfg
+
+cp -r src/ docker/entity-api/src/
+
+docker-compose -f docker/docker-compose.test.yml run --rm entity-api-test sh -c "pytest --disable-warnings"
+# tear down the test containers
+docker-compose -f docker/docker-compose.test.yml down
+
+# if there's a src/instance/app.cfg.bak file, revert it to src/instance/app.cfg
+if [ -f src/instance/app.cfg.bak ]; then
+    echo "Restoring src/instance/app.cfg from src/instance/app.cfg.bak"
+    mv src/instance/app.cfg.bak src/instance/app.cfg
+fi
---
+++ test/helpers/database.py
@@ -0,0 +1,105 @@
+import time
+
+import pytest
+from neo4j import GraphDatabase
+
+from . import GROUP
+
+
+def wait_for_neo4j(uri, user, password, timeout=60):
+    """Wait for Neo4j to be ready
+
+    Parameters
+    ----------
+    uri : str
+        URI of the Neo4j server
+    user : str
+        Username to connect to the Neo4j server
+    password : str
+        Password to connect to the Neo4j server
+    timeout : int
+        Timeout in seconds (default is 60 seconds)
+
+    Returns
+    -------
+    driver : neo4j.GraphDatabase.driver
+        Neo4j driver object
+    """
+    driver = GraphDatabase.driver(uri, auth=(user, password))
+    start_time = time.time()
+    while True:
+        try:
+            with driver.session() as session:
+                session.run("RETURN 1")
+            print("Neo4j is up and running")
+            break
+        except Exception as e:
+            if time.time() - start_time > timeout:
+                print("Timeout waiting for Neo4j to be ready")
+                raise e
+            print("Waiting for Neo4j to be ready...")
+            time.sleep(1)
+
+    return driver
+
+
+@pytest.fixture(scope="session")
+def db_session():
+    """Test fixture to create a Neo4j session
+
+    Returns
+    -------
+    session : neo4j.Session
+        Neo4j session object. Fixture takes care of closing the session.
+    """
+    neo4j_uri = "bolt://neo4j-test:7687"
+    neo4j_username = "neo4j"
+    neo4j_password = None
+
+    driver = wait_for_neo4j(neo4j_uri, neo4j_username, neo4j_password)
+    session = driver.session()
+    yield session
+    session.close()
+    driver.close()
+
+
+@pytest.fixture(scope="session")
+def lab(db_session):
+    """Test fixture to create a lab node in the Neo4j database
+
+    Parameters
+    ----------
+    db_session : neo4j.Session
+        Neo4j session object from the `db_session` fixture
+
+    Returns
+    -------
+    lab : dict
+        Lab node properties
+    """
+    lab = {
+        "entity_type": "Lab",
+        "last_modified_timestamp": 1661717122681,
+        "displayname": GROUP["displayname"],
+        "created_timestamp": 1661717122681,
+        "label": GROUP["name"],
+        "uuid": GROUP["uuid"],
+    }
+
+    # Create a lab node if it doesn't exist
+    query = "MATCH (l:Lab {uuid: $uuid}) RETURN l"
+    result = db_session.run(query, uuid=lab["uuid"])
+    if result.single() is None:
+        query = """
+        CREATE (l:Lab {
+            entity_type: $entity_type,
+            last_modified_timestamp: $last_modified_timestamp,
+            displayname: $displayname,
+            created_timestamp: $created_timestamp,
+            label: $label,
+            uuid: $uuid
+        })
+        """
+        db_session.run(query, **lab)
+
+    yield lab
