743bf26a9b27b616664b9ec1490ea8922985598d
Tyler Madonna <tjm159@pitt.edu>
2025-02-17 15:00:20 -0500

    Adding unauthed tests for getting entities

--- test/test_get_entities.py
+++ test/test_get_entities.py
@@ -75,6 +75,52 @@ def test_get_source_by_sennet_id(db_session, app, requests):
         assert res.json["data_access_level"] == "consortium"
 
 
+def test_get_source_by_uuid_no_auth(db_session, app, requests):
+    # Create provenance in test database
+    test_entities = create_provenance(db_session, ["source"])
+    test_source = test_entities["source"]
+
+    # uuid mock responses
+    uuid_api_url = app.config["UUID_API_URL"]
+    requests.add_response(
+        f"{uuid_api_url}/uuid/{test_source['uuid']}",
+        "get",
+        mock_response(200, {k: test_source[k] for k in ["uuid", "sennet_id", "base_id"]}),
+    )
+
+    with app.test_client() as client:
+        res = client.get(
+            f"/entities/{test_source['uuid']}",
+        )
+
+        assert res.status_code == 403
+
+
+def test_get_source_by_uuid_public_no_auth(db_session, app, requests):
+    # Create provenance in test database
+    test_entities = create_provenance(db_session, [
+        {"type": "source", "data_access_level": "public"}
+    ])
+    test_source = test_entities["source"]
+
+    # uuid mock responses
+    uuid_api_url = app.config["UUID_API_URL"]
+    requests.add_response(
+        f"{uuid_api_url}/uuid/{test_source['uuid']}",
+        "get",
+        mock_response(200, {k: test_source[k] for k in ["uuid", "sennet_id", "base_id"]}),
+    )
+
+    with app.test_client() as client:
+        res = client.get(
+            f"/entities/{test_source['uuid']}",
+        )
+
+        assert res.status_code == 200
+        assert res.json["data_access_level"] == "public"
+        assert not any(k.startswith("lab_") for k in res.json.keys())  # no lab id fields
+
+
 def test_get_organ_sample_by_uuid(db_session, app, requests):
     # Create provenance in test database
     test_entities = create_provenance(db_session, ["source", "organ"])
@@ -153,6 +199,53 @@ def test_get_organ_sample_by_sennet_id(db_session, app, requests):
         assert res.json["created_by_user_email"] == USER["email"]
         assert res.json["created_by_user_sub"] == USER["sub"]
         assert res.json["data_access_level"] == "consortium"
+
+
+def test_get_organ_sample_by_uuid_no_auth(db_session, app, requests):
+    # Create provenance in test database
+    test_entities = create_provenance(db_session, ["source", "organ"])
+    test_organ = test_entities["organ"]
+
+    # uuid mock responses
+    uuid_api_url = app.config["UUID_API_URL"]
+    requests.add_response(
+        f"{uuid_api_url}/uuid/{test_organ['uuid']}",
+        "get",
+        mock_response(200, {k: test_organ[k] for k in ["uuid", "sennet_id", "base_id"]}),
+    )
+
+    with app.test_client() as client:
+        res = client.get(
+            f"/entities/{test_organ['uuid']}",
+        )
+
+        assert res.status_code == 403
+
+
+def test_get_organ_sample_by_uuid_public_no_auth(db_session, app, requests):
+    # Create provenance in test database
+    test_entities = create_provenance(db_session, [
+        {"type": "source", "data_access_level": "public"},
+        {"type": "organ", "data_access_level": "public"}
+    ])
+    test_organ = test_entities["organ"]
+
+    # uuid mock responses
+    uuid_api_url = app.config["UUID_API_URL"]
+    requests.add_response(
+        f"{uuid_api_url}/uuid/{test_organ['uuid']}",
+        "get",
+        mock_response(200, {k: test_organ[k] for k in ["uuid", "sennet_id", "base_id"]}),
+    )
+
+    with app.test_client() as client:
+        res = client.get(
+            f"/entities/{test_organ['uuid']}",
+        )
+
+        assert res.status_code == 200
+        assert res.json["data_access_level"] == "public"
+        assert not any(k.startswith("lab_") for k in res.json.keys())  # no lab id fields
 
 
 def test_get_block_sample_by_uuid(db_session, app, requests):
@@ -235,6 +328,54 @@ def test_get_block_sample_by_sennet_id(db_session, app, requests):
         assert res.json["data_access_level"] == "consortium"
 
 
+def test_get_block_sample_by_uuid_no_auth(db_session, app, requests):
+    # Create provenance in test database
+    test_entities = create_provenance(db_session, ["source", "organ", "block"])
+    test_block = test_entities["block"]
+
+    # uuid mock responses
+    uuid_api_url = app.config["UUID_API_URL"]
+    requests.add_response(
+        f"{uuid_api_url}/uuid/{test_block['uuid']}",
+        "get",
+        mock_response(200, {k: test_block[k] for k in ["uuid", "sennet_id", "base_id"]}),
+    )
+
+    with app.test_client() as client:
+        res = client.get(
+            f"/entities/{test_block['uuid']}",
+        )
+
+        assert res.status_code == 403
+
+
+def test_get_block_sample_by_uuid_public_no_auth(db_session, app, requests):
+    # Create provenance in test database
+    test_entities = create_provenance(db_session, [
+        {"type": "source", "data_access_level": "public"},
+        {"type": "organ", "data_access_level": "public"},
+        {"type": "block", "data_access_level": "public"}
+    ])
+    test_block = test_entities["block"]
+
+    # uuid mock responses
+    uuid_api_url = app.config["UUID_API_URL"]
+    requests.add_response(
+        f"{uuid_api_url}/uuid/{test_block['uuid']}",
+        "get",
+        mock_response(200, {k: test_block[k] for k in ["uuid", "sennet_id", "base_id"]}),
+    )
+
+    with app.test_client() as client:
+        res = client.get(
+            f"/entities/{test_block['uuid']}",
+        )
+
+        assert res.status_code == 200
+        assert res.json["data_access_level"] == "public"
+        assert not any(k.startswith("lab_") for k in res.json.keys())  # no lab id fields
+
+
 def test_get_section_sample_by_uuid(db_session, app, requests):
     # Create provenance in test database
     test_entities = create_provenance(db_session, ["source", "organ", "block", "section"])
@@ -313,6 +454,55 @@ def test_get_section_sample_by_sennet_id(db_session, app, requests):
         assert res.json["created_by_user_email"] == USER["email"]
         assert res.json["created_by_user_sub"] == USER["sub"]
         assert res.json["data_access_level"] == "consortium"
+
+
+def test_get_section_sample_by_uuid_no_auth(db_session, app, requests):
+    # Create provenance in test database
+    test_entities = create_provenance(db_session, ["source", "organ", "block", "section"])
+    test_section = test_entities["section"]
+
+    # uuid mock responses
+    uuid_api_url = app.config["UUID_API_URL"]
+    requests.add_response(
+        f"{uuid_api_url}/uuid/{test_section['uuid']}",
+        "get",
+        mock_response(200, {k: test_section[k] for k in ["uuid", "sennet_id", "base_id"]}),
+    )
+
+    with app.test_client() as client:
+        res = client.get(
+            f"/entities/{test_section['uuid']}",
+        )
+
+        assert res.status_code == 403
+
+
+def test_get_section_sample_by_uuid_public_no_auth(db_session, app, requests):
+    # Create provenance in test database
+    test_entities = create_provenance(db_session, [
+        {"type": "source", "data_access_level": "public"},
+        {"type": "organ", "data_access_level": "public"},
+        {"type": "block", "data_access_level": "public"},
+        {"type": "section", "data_access_level": "public"}
+    ])
+    test_section = test_entities["section"]
+
+    # uuid mock responses
+    uuid_api_url = app.config["UUID_API_URL"]
+    requests.add_response(
+        f"{uuid_api_url}/uuid/{test_section['uuid']}",
+        "get",
+        mock_response(200, {k: test_section[k] for k in ["uuid", "sennet_id", "base_id"]}),
+    )
+
+    with app.test_client() as client:
+        res = client.get(
+            f"/entities/{test_section['uuid']}",
+        )
+
+        assert res.status_code == 200
+        assert res.json["data_access_level"] == "public"
+        assert not any(k.startswith("lab_") for k in res.json.keys())  # no lab id fields
 
 
 def test_get_dataset_by_uuid(db_session, app, requests):
@@ -399,3 +589,54 @@ def test_get_dataset_by_sennet_id(db_session, app, requests):
         assert res.json["created_by_user_email"] == USER["email"]
         assert res.json["created_by_user_sub"] == USER["sub"]
         assert res.json["data_access_level"] == "consortium"
+
+
+def test_get_dataset_by_uuid_no_auth(db_session, app, requests):
+    # Create provenance in test database
+    test_entities = create_provenance(db_session, ["source", "organ", "block", "section"])
+    test_section = test_entities["section"]
+
+    # uuid mock responses
+    uuid_api_url = app.config["UUID_API_URL"]
+    requests.add_response(
+        f"{uuid_api_url}/uuid/{test_section['uuid']}",
+        "get",
+        mock_response(200, {k: test_section[k] for k in ["uuid", "sennet_id", "base_id"]}),
+    )
+
+    with app.test_client() as client:
+        res = client.get(
+            f"/entities/{test_section['uuid']}",
+        )
+
+        assert res.status_code == 403
+
+
+def test_get_dataset_by_uuid_published_no_auth(db_session, app, requests):
+    # Create provenance in test database
+    test_entities = create_provenance(db_session, [
+        {"type": "source", "data_access_level": "public"},
+        {"type": "organ", "data_access_level": "public"},
+        {"type": "block", "data_access_level": "public"},
+        {"type": "section", "data_access_level": "public"},
+        {"type": "dataset", "data_access_level": "public", "status": "Published"}
+    ])
+    test_dataset = test_entities["dataset"]
+
+    # uuid mock responses
+    uuid_api_url = app.config["UUID_API_URL"]
+    requests.add_response(
+        f"{uuid_api_url}/uuid/{test_dataset['uuid']}",
+        "get",
+        mock_response(200, {k: test_dataset[k] for k in ["uuid", "sennet_id", "base_id"]}),
+    )
+
+    with app.test_client() as client:
+        res = client.get(
+            f"/entities/{test_dataset['uuid']}",
+        )
+
+        assert res.status_code == 200
+        assert res.json["data_access_level"] == "public"
+        assert res.json["status"] == "Published"
+        assert not any(k.startswith("lab_") for k in res.json.keys())  # no lab id fields
